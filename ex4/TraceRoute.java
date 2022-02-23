a50061ca9d64f34275fa649ec3082b1f6531cae4
valimai62 <100309643+valimai62@users.noreply.github.com>
2022-02-24 00:49:06 +0530

    Update TraceRoute.java

--- ex4/TraceRoute.java
+++ ex4/TraceRoute.java
@@ -7,14 +7,15 @@ import java.util.*;
 
 public class TraceRoute {
     public static void main(String[] args) {
+        System.out.println("Enter a host name : ");
         Scanner sc = new Scanner(System.in);
         String ip;
         ip=sc.nextLine();
-        //ping www.google.com
+        //tracert www.google.com
 
         try{
             Runtime r = Runtime.getRuntime();
-            Process p =r.exec(ip);
+            Process p =r.exec("tracert "+ ip);
             BufferedReader inStream = new BufferedReader(new InputStreamReader(p.getInputStream()));
             String s;
             while((s=inStream.readLine())!=null){
